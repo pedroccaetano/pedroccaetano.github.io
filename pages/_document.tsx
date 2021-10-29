a22c377a58121839eac5f93646b6bf797756e337
Pedro Caetano <pedroccaetano@live.com>
2021-10-29 20:38:02 -0300

    fix Permissions-Poliyc interest-cohort

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta http-equiv="Permissions-Policy" content="interest-cohort=()"/>
           <link rel="icon" href="/favicon.ico" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
